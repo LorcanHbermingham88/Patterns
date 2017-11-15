3888a733740d9c8ffa045b7cb1afdb3499b6db9f
lorcan <healylorcan88@gmail.com>
2017-11-15 16:58:47 +0000

    factory pattern shapes made and each draw method called

---
+++ FactoryPattern/src/Circle.java
@@ -0,0 +1,7 @@
+public class Circle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.print("Inside Circle::Draw() method.");
+    }
+}
---
+++ FactoryPattern/src/Square.java
@@ -0,0 +1,7 @@
+public class Square implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.print("Inside the square:: draw() method.");
+    }
+}
