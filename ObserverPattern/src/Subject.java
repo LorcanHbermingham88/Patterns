1357f9c19ee05166ff8db19d5c9f40dd620694a9
lorcan <healylorcan88@gmail.com>
2017-11-16 12:03:09 +0000

    subject setup. observer class started

--- ObserverPattern/src/Subject.java
+++ ObserverPattern/src/Subject.java
@@ -9,6 +9,17 @@ public class Subject {
     public int getState(){
         return state;
     }
-    
 
+    public void setState(int state) {
+        this.state = state;
+        notifyAllObservers();
+    }
+    public void attach(Observer observer){
+        observers.add(observer);
+    }
+    public void notifyAllObservers(){
+        for(Observer observer : observers) {
+            observer.update();
+        }
+    }
 }
